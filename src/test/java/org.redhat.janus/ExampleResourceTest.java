cc7c139fdadc601857bf5ba07b428e2b6226bf54
Michael Thirion <mthirion@redhat.com>
2025-04-18 15:14:09 +0200

    Update ExampleResourceTest.java
    Fix unit test

--- src/test/java/org.redhat.janus/ExampleResourceTest.java
+++ src/test/java/org.redhat.janus/ExampleResourceTest.java
@@ -9,11 +9,11 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 public class ExampleResourceTest {
 
-    private String michael="{\"id\": 1, \"name\": \"Michael\", \"lastname\": \"Thirion\", \"email\": \"mthirion@redhat.com\"}";
-    private String rachid="{\"id\": 2, \"name\": \"Rachid\", \"lastname\": \"Snoussi\", \"email\": \"snoussi@redhat.com\"}";
+    private String michael="{\"id\": \"1\", \"name\": \"Michael\", \"lastname\": \"Thirion\", \"email\": \"mthirion@redhat.com\"}";
+    private String rachid="{\"id\": \"2\", \"name\": \"Rachid\", \"lastname\": \"Snoussi\", \"email\": \"snoussi@redhat.com\"}";
 
     @Test
-    public void testHelloEndpoint() {
+    public void testEndpoint() {
         given()
                 .when().get("/customers/1")
                 .then()
